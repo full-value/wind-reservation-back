dee68d79cd32500eed8f5f06adb3ee3dd1e03b6e
webstar923 <webstar1987923@hotmail.com>
2025-03-12 16:12:46 +0900

    test1.1

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 // Set up Sequelize with your MySQL credentials
-const sequelize = new Sequelize('reservation_wing_system', 'root', '', {
+const sequelize = new Sequelize('reservation_wing_system', 'root', 'GameT!me0106', {
+// const sequelize = new Sequelize('reservation_wing_system', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql',
 });
